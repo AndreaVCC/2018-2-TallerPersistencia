e7c5a361e00314b71fb042bc23c1452db33f3889
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2019-01-31 16:52:26 -0300

    Taller de firebase y autenticación

---
+++ src/js/auth/auth.js
@@ -0,0 +1,21 @@
+export const checkAuthState = (callback) => {
+  firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+      console.log("Hay un usuario > "+JSON.stringify(user));
+      callback(user);
+    }else{
+      console.log("No está logueado");
+      callback(null);
+    }
+  })
+};
+
+export const registerUser = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user)=>{
+      console.log("Usuario registrado > "+JSON.stringify(user));
+    })
+    .catch((error) => {
+      console.error("Error > "+error.message);
+    });
+}
